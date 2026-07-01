91fe150405c0045cd91640ae8481f98310b034dc
Sestepa Design <sestepadesign@gmail.com>
2026-07-01 14:34:41 -0300

    Perf: migrate tracking scripts to Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://design.sestepa.com',
   integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+      },
+    }),
     sitemap({
       i18n: {
         defaultLocale: 'en',
