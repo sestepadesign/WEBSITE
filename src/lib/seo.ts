33728d77471012691fd4e17ad5ccd87a61b33d3c
Sestepa Design <sestepadesign@gmail.com>
2026-07-09 12:27:11 -0300

    fix: integrate GA4 tag into centralized SEO configuration and Layout

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -5,6 +5,7 @@ export { SITE_LOCALES, DEFAULT_LOCALE, type SiteLocale } from './locale';
 export const GTM_ID = 'GTM-K6MSRJX9';
 export const GOOGLE_ADS_ID = 'AW-795871100';
 export const GTAG_ID = 'GT-WKTZS5C';
+export const GA4_ID = 'G-DZ9ZSG3VH0';
 export const COOKIEYES_CLIENT_ID = 'f292b5d0baace10a4365b956';
 export const METRICOOL_HASH = '4514d2b6d34c58b6a0b6a1c1649df7b';
 
