fd9975401964e07db39d4293970dd4fe66bc0ce4
Sestepa Design <sestepadesign@gmail.com>
2026-07-24 11:28:48 -0300

    Remove old terrace garden images from Santa Eugenia project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -611,18 +611,7 @@ export const projects: Project[] = [
       "garden design mallorca santa eugenia sestepa landscape design (2).jpg",
       "garden design mallorca santa eugenia sestepa landscape design (4).jpg",
       "garden design mallorca santa eugenia sestepa landscape design (6).jpg",
-      "garden design mallorca santa eugenia sestepa landscape design (7).jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-10.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-11.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-12.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-2.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-4.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-5.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-6.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-7.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-8.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca-9.jpg",
-      "Terrace-Garden-Santa-Eugenia-SEstepa-Design-Mallorca.jpg"
+      "garden design mallorca santa eugenia sestepa landscape design (7).jpg"
     ],
     en: {
       title: "Santa Eugènia",
